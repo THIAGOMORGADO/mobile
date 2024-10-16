4cc7fb3e0a42b8cee753117295ee5a923f65fd3b
thiago morgado <thiago_morgado@hotmail.com>
2024-10-16 03:55:16 -0300

    Estruturando  icons e theme em arquivo global

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Loading from './src/components/loading';
+import { useEffect, useState } from 'react';
+import Signin from './src/screens/Signin';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 3000);
+
+
   return (
     <>
       <StatusBar hidden />
-      <View >
-        <Text>Open up App.js to start working on your app!</Text>
 
-      </View>
-      {/* <Loading /> */}
+      {loading ? <Loading /> : <Signin />}
     </>
 
   );
---
+++ src/screens/Signin/index.jsx
@@ -0,0 +1,11 @@
+import { Image, Text, View } from 'react-native';
+import icons from '../../constants/icons';
+
+export default function Signin() {
+  return (
+    <View>
+
+      <Text>SignIN</Text>
+    </View>
+  );
+}
