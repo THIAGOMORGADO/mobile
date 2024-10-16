c80821a0e26d90e59447a5c3ac4a9d814e3b4f1e
thiago morgado <thiago_morgado@hotmail.com>
2024-10-16 03:55:29 -0300

    montando estruturas

---
+++ src/constants/icons.js
@@ -0,0 +1,10 @@
+import Calendar from '../assets/calendar.png'
+import Clock from '../assets/clock.png'
+import Famele from '../assets/female.png'
+import Home from '../assets/home.png'
+import Logo from '../assets/logo.png'
+import Male from '../assets/male.png'
+import Profile from '../assets/profile.png'
+
+
+export default { Calendar, Clock, Famele, Home, Logo, Male, Profile };
